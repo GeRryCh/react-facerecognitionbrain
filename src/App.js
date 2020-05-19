a636e345299d053ef3d4b5f9bcd771afb292f52d
German Velibekov <german.velibekov@gmail.com>
2020-05-19 17:32:10 +0300

    connect to clarifai API

--- src/App.js
+++ src/App.js
@@ -1,26 +1,72 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
+import Rank from './components/Rank/Rank';
+import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
+import 'tachyons';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const app = new Clarifai.App({
+  apiKey: 'c8e67b53e6fd47ec90169a8085beb694'
+});
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 80,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
+};
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      imageUrl: ''
+    };
+  }
+
+  onInputChange = (event) => {
+    this.setState({ input: event.target.value });
+  };
+
+  onSubmit = () => {
+    this.setState({ imageUrl: this.state.input });
+    app.models.initModel(Clarifai.GENERAL_MODEL)
+      .then(model => {
+        return model.predict("https://clarifai.com/cms-assets/20180320225539/general-009.jpg");
+      })
+      .then(response => {
+        const data = response['outputs'][0]['data'];
+        console.log('data', data);
+      });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Particles
+          className='particles'
+          params={particlesOptions}
+        />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm
+          onInputChange={this.onInputChange}
+          onSubmit={this.onSubmit} />
+        <FaceRecognition imageUrl={this.state.imageUrl} />}
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,17 @@
+import React, { Fragment } from 'react';
+
+const Rank = () => {
+  return (
+    <Fragment>
+      <div className='white f3'>
+        {'Andrei, your current rank is....'}
+      </div>
+      <div className='white f1'>
+        {'#5'}
+      </div>
+    </Fragment>
+  );
+};
+
+export default Rank;
+
