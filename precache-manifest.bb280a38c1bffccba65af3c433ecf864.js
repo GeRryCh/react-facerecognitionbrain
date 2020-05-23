c0e21576efce90a09bdf299fb6ad05b2664a49fa
German Velibekov <german.velibekov@gmail.com>
2020-05-23 21:32:29 +0300

    Updates

---
+++ precache-manifest.bb280a38c1bffccba65af3c433ecf864.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24e8b0f253cc8c9899ae9e2c7835e637",
+    "url": "/react-facerecognitionbrain/index.html"
+  },
+  {
+    "revision": "6056b6c31753bf73d952",
+    "url": "/react-facerecognitionbrain/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "b64ca2de5453f910425b",
+    "url": "/react-facerecognitionbrain/static/css/main.6c096beb.chunk.css"
+  },
+  {
+    "revision": "6056b6c31753bf73d952",
+    "url": "/react-facerecognitionbrain/static/js/2.e2c060f3.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/react-facerecognitionbrain/static/js/2.e2c060f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b64ca2de5453f910425b",
+    "url": "/react-facerecognitionbrain/static/js/main.839ca698.chunk.js"
+  },
+  {
+    "revision": "c61d86de0ee420524edb",
+    "url": "/react-facerecognitionbrain/static/js/runtime-main.0e84a851.js"
+  }
+]);
